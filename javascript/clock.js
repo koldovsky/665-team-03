6481b5b10b6d5bf6f8fb278a1d13a5f78ffd53dc
Prybula <prybulanick@gmail.com>
2022-02-14 12:50:31 +0000

    added js element Clock to why choose block

---
+++ javascript/clock.js
@@ -0,0 +1,9 @@
+(function () {
+
+    function updateClock() {
+        const clockContainer = document.querySelector('.clock');
+        clockContainer.textContent = new Date().toLocaleTimeString();
+    }
+
+    setInterval(updateClock, 1000);
+})();
